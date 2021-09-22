1794b51fb4adede4f4a58e230fb06eb20fb21172
valeriia-radzivilo <89969040+valeriia-radzivilo@users.noreply.github.com>
2021-09-22 17:35:17 +0300

    Add files via upload

--- 1-25.py
+++ 1-25.py
@@ -1,5 +1,4 @@
-import math
 hyp = float(input("гіпотенуза = "))
 leg1 = float(input("перший катет = "))
-leg2 = gip**2 - cat1**2
-print("другий катет =", math.sqrt(cat2))
+leg2 = hyp**2 - leg1**2
+print("другий катет =", leg2**0.5)
