a7d5f960a2e3d08860f7991cb7741ad07c515269
valeriia-radzivilo <89969040+valeriia-radzivilo@users.noreply.github.com>
2021-09-30 08:45:41 +0300

    Add files via upload

--- 1-25.cpp
+++ 1-25.cpp
@@ -9,10 +9,10 @@ int main()
     cout << "hypotenuse=";
     cin >> hyp;
     //перший катет
-    cout << "leg 1=";
+    cout << "leg 1 = ";
     cin >> leg1;
     // другий катет
     float leg2 =pow( pow(hyp,2.0)-pow(leg1,2.0), 0.5);
-    cout << "leg 2 ="<< leg2;
+    cout << "leg 2 = "<< leg2;
     return 0 ;
 }
--- 1-25.py
+++ 1-25.py
@@ -1,4 +1,4 @@
 hyp = float(input("гіпотенуза = "))
 leg1 = float(input("перший катет = "))
-leg2 = hyp**2 - leg1**2
-print("другий катет =", leg2**0.5)
+leg2 = (hyp**2 - leg1**2) ** 0.5
+print("другий катет =", leg2)
